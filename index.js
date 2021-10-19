b3ccf6e72b8090c04933ea8a3cc7b0dac4dc1522
unknown <juancarlos@codika.net>
2021-10-19 02:50:14 -0400

    make it better

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const Nightmare = require('nightmare')
 
 console.log('Nightmare script started')
 console.log('==========')
+const rncUrl = "https://dgii.gov.do/app/WebApps/ConsultasWeb2/ConsultasWeb/consultas/rnc.aspx";
 
-;(async () => {
-	await nightmare
-		.goto('https://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/')
-		.wait('.post-title')
+;(async () => { 
+	
+const	result = await  nightmare
+	.goto(rncUrl)
+	.wait("body")
+	.type("#cphMain_txtRNCCedula", '40223486859')
+	.click("#cphMain_btnBuscarPorRNC")
+	.wait(() => {
+		const tbody = document.querySelector("tbody");
+		const lblIn = document.querySelector("#cphMain_lblInformacion");
+		return (!(tbody === null && lblIn.innerText === ""))})
+	.evaluate(() => document.querySelector("body").innerHTML)
+	.end()
+   
 
-	console.log(
-		await nightmare.evaluate(() =>
-			document.querySelector('.post-title').textContent
-		)
-	)
-	console.log('=========')
+
+	console.log(result)
 	console.log('All done')
 
 	await nightmare.end().catch(error => console.error(error))
